4c1a93b3cad882a2c0ebacc6761b4fb9a69bfcaa
hazraf2009 <73149382+hazraf2009@users.noreply.github.com>
2024-06-09 17:16:35 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from main import generate, math, quotes
+
+with open("token.txt", "r") as f:
+    token = f.read()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx, long = 10):
+    await ctx.send(generate(long))
+
+bot.run(token)
